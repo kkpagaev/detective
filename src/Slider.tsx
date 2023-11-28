ef2119ffa6b17b7d9bda5fb78b251dca846649d3
kkpagaev <kkpagaev@gmail.com>
2023-11-28 19:18:01 +0200

    more slider styles

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,7 +1,52 @@
 import { useAppContext } from "./context/app-context";
 
+function getTime(time: number) {
+  switch (time) {
+    case 0:
+      return "9:00";
+    case 1:
+      return "9:30";
+    case 2:
+      return "10:00";
+    case 3:
+      return "10:30";
+    case 4:
+      return "11:00";
+    case 5:
+      return "11:30";
+    case 6:
+      return "12:00";
+    case 7:
+      return "12:30";
+    case 8:
+      return "13:00";
+    case 9:
+      return "13:30";
+    case 10:
+      return "14:00";
+    case 11:
+      return "14:30";
+    case 12:
+      return "15:00";
+    case 13:
+      return "15:30";
+    case 14:
+      return "16:00";
+    case 15:
+      return "16:30";
+    case 16:
+      return "17:00";
+    case 17:
+      return "17:30";
+    case 18:
+      return "18:00";
+    case 19:
+      return "18:30";
+  }
+}
+
 export const Slider = () => {
-  const {state, setState} = useAppContext();
+  const { state, setState } = useAppContext();
 
   const hasHint = state.serverRoomChoise == "video"
 
@@ -10,8 +55,13 @@ export const Slider = () => {
   }
 
   return <div>
-    <div className="relative mb-6 text-lg text-black">
-      <label htmlFor="steps-range" className="block mb-2 text-lg font-medium text-gray-900 ">Оберіть час перегляду</label>
+    <div className="relative md:text-lg text-sm text-black">
+      <div className="flex justify-between pr-8 pl-8">
+        <h2 className="block mb-6">
+          <span className="text-3xl font-bold">Виберіть час перегляду</span>
+        </h2>
+        <span className="text-3xl font-bold">Час: {getTime(2*state.time)}</span>
+      </div>
       <input onChange={(e) => setTime(+e.target.value)} value={state.time} id="steps-range" type="range" min="0" max="8" step="0.5" className="w-full h-6 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
       <span className=" absolute start-0 -bottom-6"
         style={{
@@ -61,13 +111,13 @@ export const Slider = () => {
       >15:00</span>
 
 
-      <span className="text-gray-500 absolute -translate-x-1/2 rtl:translate-x-1/2 -bottom-6"
+      <span className="absolute -translate-x-1/2 rtl:translate-x-1/2 -bottom-6"
         style={{
           insetInlineStart: "calc(7/8 * 100%)"
         }}
       >16:00</span>
 
-      <span className="text-gray-500 absolute end-0 -bottom-6">17:00</span>
+      <span className="absolute end-0 -bottom-6">17:00</span>
     </div>
   </div>
 }
